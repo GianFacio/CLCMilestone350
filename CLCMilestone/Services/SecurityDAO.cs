4c057299292ccff1e83c8391f46704ec7311dd76
gianfacio <gianfaciofilms@gmail.com>
2022-10-02 15:53:15 -0700

    new commit

--- CLCMilestone/Services/SecurityDAO.cs
+++ CLCMilestone/Services/SecurityDAO.cs
@@ -1,23 +1,22 @@
-﻿using CLCMilestone.Models;
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+using System.Security.Permissions;
 using System.Data.SqlClient;
-
-
-
-
+using CLCMilestone.Models;
 
 namespace CLCMilestone.Services
 {
     public class SecurityDAO
     {
-        
-        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CLC;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-        public bool FindUserByNameAndPassword(Login User)
+        public bool FindUserByNameAndPassword(Login user)
         {
             // Assume nothing is found
             bool success = false;
 
             // Uses prepared statements for security. @username @password are defined below
+
             string sqlStatement = "SELECT * FROM dbo.Users WHERE username = @username and password = @password";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -25,8 +24,8 @@ namespace CLCMilestone.Services
                 SqlCommand command = new SqlCommand(sqlStatement, connection);
 
                 // Define the values of the two placeholders in the sqlStatement string
-                command.Parameters.Add("@USERNAME", System.Data.SqlDbType.VarChar, 50).Value = User.Username;
-                command.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarChar, 50).Value = User.Password;
+                command.Parameters.Add("@USERNAME", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
+                command.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
 
                 try
                 {
@@ -41,7 +40,7 @@ namespace CLCMilestone.Services
                     Console.WriteLine(ex.Message);
                 };
             }
-            return success;
+        return success;
         }
     }
 }
